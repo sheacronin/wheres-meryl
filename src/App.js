65f9a399493d36ee0e37298377538f0df1c5bd7d
Shea Cronin <shealcronin@gmail.com>
2021-11-10 18:37:07 -0500

    Add initial image and component for the image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import WheresMerylImage from './components/WheresMerylImage';
 import app from './firebaseInit';
+import wheresMeryl1 from './img/wheres-meryl-1.jpg';
 
 function App() {
-    return <div>Hello world</div>;
+    return (
+        <div>
+            Hello world
+            <WheresMerylImage imageSrc={wheresMeryl1} />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/WheresMerylImage.js
@@ -0,0 +1,8 @@
+import '../styles/WheresMerylImage.css';
+
+function WheresMerylImage(props) {
+    const { imageSrc } = props;
+    return <img className="wheres-meryl" src={imageSrc}></img>;
+}
+
+export default WheresMerylImage;
