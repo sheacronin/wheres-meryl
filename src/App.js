16a8f90b3b42099cc517e02718528dc5f6cd0368
Shea Cronin <shealcronin@gmail.com>
2021-11-15 08:42:34 -0500

    Add Timer that begins on App mount

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useState } from 'react/cjs/react.development';
+import { useEffect, useState } from 'react/cjs/react.development';
 import './App.css';
+import Timer from './components/Timer';
 import WheresMerylImage from './components/WheresMerylImage';
 // eslint-disable-next-line no-unused-vars
 import app from './firebaseInit';
@@ -7,6 +8,11 @@ import wheresMeryl1 from './img/wheres-meryl-1.jpg';
 
 function App() {
     const [foundCharacters, setFoundCharacters] = useState([]);
+    const [timeInSeconds, setTimeInSeconds] = useState(0);
+
+    useEffect(() => {
+        setInterval(() => setTimeInSeconds((prevState) => prevState + 1), 1000);
+    }, []);
 
     return (
         <div>
@@ -16,6 +22,7 @@ function App() {
                     Scroll around this image of Greece to find Donna and the
                     Dynamos from hit film Mamma Mia!
                 </p>
+                <Timer timeInSeconds={timeInSeconds} />
             </header>
             <WheresMerylImage
                 imageSrc={wheresMeryl1}
---
+++ src/components/Timer.js
@@ -0,0 +1,31 @@
+function Timer(props) {
+    const { timeInSeconds } = props;
+
+    function convertTimeInSeconds() {
+        let minutes;
+        let seconds = timeInSeconds;
+
+        if (timeInSeconds > 60) {
+            minutes = Math.floor(timeInSeconds / 60);
+            seconds = timeInSeconds % 60;
+        }
+
+        if (minutes < 10) {
+            minutes = addZeroBefore(minutes);
+        }
+
+        if (seconds < 10) {
+            seconds = addZeroBefore(seconds);
+        }
+
+        return `${minutes}:${seconds}`;
+
+        function addZeroBefore(num) {
+            return '0' + num;
+        }
+    }
+
+    return <div>{convertTimeInSeconds()}</div>;
+}
+
+export default Timer;
